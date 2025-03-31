b7d8e0a1dd42d3f743906f513fd68bc528ce6936
Brent <b.mercado@student.fdu.edu>
2025-03-31 16:38:57 -0400

    added documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+/*Resources used V1:
+https://react.dev/learn
+https://chatgpt.com/
+https://www.youtube.com/watch?v=5fiXEGdEK10&t=1105s -> App structure based off this tutorial
+*/
+
+
 import Icon from "./Components/icon"
 
 const App = () => {
   return (
     <div className = "container">
-      <div className = "chatbot-popup">
-        <div className = "chat-header">
-          <div className = "header-info">
+      <div className = "chatbot-popup"> {/*Chat Message Screen Div*/}
+        <div className = "chat-header"> {/*Chat header Div*/}
+          <div className = "header-info"> {/*Chat header contents*/}
             <Icon />
             <h2 className = "logo-t">Explain this in NBA terms</h2>
           </div>
@@ -13,22 +20,22 @@ const App = () => {
         <span className="material-symbols-outlined">circle</span> </button> 
         </div>
 
-        <div className="chat-body"> {/*Body*/}
-          <div className ="message message-l">
-          <p className = "message-text">
+        <div className="chat-body"> {/*Body of chat bot*/} 
+          <div className ="message message-l"> {/*chat elements from chatbot, left-hand*/} 
+          <p className = "message-text"> {/*contents*/} 
             Placeholder bot<br />
           </p>
-          <div className ="message message-r">
+          <div className ="message message-r"> {/*chat elemets from user, right-hand*/} 
           <p className = "message-text">
             placeholder Input <br /> 
           </p>
           
           </div>
           </div>
-        <div className="chat-foot"> {/*footer*/}
-        <form action="#" className="chat-form">
+        <div className="chat-foot"> {/*begin footer, body of where message is inputed by user*/}
+        <form action="#" className="chat-form"> {/*message form, input*/}
           <input type="text" placeholder="Message..." 
-          className="message-input" required />
+          className="message-input" required /> {/*message form button, for submission*/}
           <button className="material-symbols-outlined">circle</button> 
         </form>
         </div>
